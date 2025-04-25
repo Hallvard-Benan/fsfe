dceb016b62e7f4d828b560cdf516662ac320da79
Hallvard <hallvard.benan@gmail.com>
2025-04-25 17:55:34 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+http.createServer(
+	function(req,res){
+		res.write("On the way to being a full stack dev");
+		res.end();
+	}
+).listen(3000);
+
+console.log("server Started");
