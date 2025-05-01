5831babc76519b689cf258ff05dd125c2faf5b00
Hallvard-Benan <hallvard.benan@gmail.com>
2025-05-01 23:03:02 +0200

    add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,15 @@ server.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
 
+process.on("SIGINT", () => {
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutDownDB();
+  });
+});
+
 // Begin Websocket
 
 const WebSocketServer = require("ws").Server;
@@ -26,6 +35,10 @@ wss.on("connection", function connection(ws) {
     ws.send("welcome to my server");
   }
 
+  db.run(
+    `INSERT INTO visitors (count, time) VALUES (${numClients}, datetime('now'))`
+  );
+
   ws.on("close", function close() {
     wss.broadCast(`Client disconnected`);
     console.log("Client disconnected");
@@ -37,3 +50,29 @@ wss.broadCast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** end websocket */
+/** begin database */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(`
+        CREATE TABLE visitors (
+            count INTEGER,
+            time TEXT
+        )`);
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutDownDB() {
+  getCounts();
+  console.log("Shutting down database");
+  db.close();
+}
